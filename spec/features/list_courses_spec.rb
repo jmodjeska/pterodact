918aa8e7e9c10a01d1eb0cda993124a1c12e7cd2
jmodjeska <cloud9@daqron.com>
2016-01-22 04:16:21 +0000

    setup first rspec test

---
+++ spec/features/list_courses_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing all courses' do
+    scenario 'returns a message when there are no courses to view' do
+        visit '/'
+        click_link 'Courses'
+
+        expect(page.url).to eq(courses_url)
+        expect(page).to have_content('0 courses')
+    end
+end
